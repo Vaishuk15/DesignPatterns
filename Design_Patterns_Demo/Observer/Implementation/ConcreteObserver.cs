96f0a10f2d599cb5a5d21796e470870c303f04f7
Vaishnavi Kanagaraj <vaishnavi.kanagaraj@groupm.com>
2025-03-23 21:01:12 +0530

    feat: observer pattern implementation added

---
+++ Design_Patterns_Demo/Observer/Implementation/ConcreteObserver.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Design_Patterns_Demo.Observer.Interface;
+
+namespace Design_Patterns_Demo.Observer.Implementation
+{
+    public class ConcreteObserver : IObserver
+    {
+        private string _name;
+        public ConcreteObserver(string name) => _name = name;
+        public void Update(string message) => Console.WriteLine($"{_name} received update: {message}");
+    }
+}
---
+++ Design_Patterns_Demo/Observer/Implementation/Subject.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Design_Patterns_Demo.Observer.Interface;
+
+namespace Design_Patterns_Demo.Observer.Implementation
+{
+    public class Subject
+    {
+        private List<IObserver> _observers = new();
+        public void Attach(IObserver observer) => _observers.Add(observer);
+        public void Detach(IObserver observer) => _observers.Remove(observer);
+        public void Notify(string message)
+        {
+            foreach (var observer in _observers)
+            {
+                observer.Update(message);
+            }
+        }
+    }
+}
